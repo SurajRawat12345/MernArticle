fb665e35741eec8a260dcea5d85cbc9d3fde17cf
SurajRawat12345 <115080037+SurajRawat12345@users.noreply.github.com>
2023-07-30 21:51:03 +0530

    final code

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 require('dotenv').config()
 const express = require('express');
-const port = process.env.PORT || 8000;
+const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Route = require('./routes/route.js');
+
 const app = express();
-const Route = require('./routes/route');
-const cors = require('cors');
+const port = process.env.PORT || 8000;
 
-// for deprication warning ........
+// Connection
 mongoose.set('strictQuery', false); 
+mongoose.connect(process.env.MONGO_URI , {useNewUrlParser : true , useUnifiedTopology : true})
+.then(console.log("Connected DB"))
 
-// Mongo DB Connection ..........
-const dburi = process.env.URI;
-mongoose.connect(dburi , {useNewUrlParser : true , useUnifiedTopology : true})
-.then( console.log("Connected to DB"));
-
-// Middleware
+// Middlewares
 app.use(cors());
-app.use(morgan('dev'));
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(express.urlencoded({extended : true}));
 
+app.get("/" , async(req,res) => {
+    res.status(200).json("Hello User")
+})
+
 // Routes
-app.use(Route);
+app.use('/art', Route);
 
 // Server Listen
 app.listen(port , () => {
